31ec89bcf28378308eeda18f2f10f4c427d5967a
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2024-01-20 02:07:17 +0900

    [Silver II] Title: 이동하기, Time: 836 ms, Memory: 39128 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,10 @@
+N, M=map(int, input().split())
+dp = [[0] * (M + 1)] * (N + 1)
+arr=[]
+for i in range(N):
+    arr.append(list(map(int, input().split())))
+
+for i in range(1, N+1):
+    for j in range(1, M+1):
+      dp[i][j]=max(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+arr[i-1][j-1]
+print(dp[N][M])
