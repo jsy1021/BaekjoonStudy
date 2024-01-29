2978f241a29afc999c668dcdd55cd9f5dd642273
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2024-01-29 17:27:33 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 436 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.py
@@ -0,0 +1,10 @@
+N=int(input())
+arr=[0]
+dp=[0]*(N+1)
+for _ in range(N):
+    arr.append(float(input()))
+dp[1]=arr[1]
+for i in range(2,N+1):
+    dp[i]=max(dp[i-1]*arr[i],arr[i])
+
+print('%0.3f' %max(dp))
