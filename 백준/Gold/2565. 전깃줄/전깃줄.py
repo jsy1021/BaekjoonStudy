88f9c754051218a25aa598260dadaa634927d93e
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2024-02-06 00:50:35 +0900

    [Gold V] Title: 전깃줄, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -0,0 +1,11 @@
+n = int(input())
+lines = [list(map(int, input().split())) for _ in range(n)]
+lines.sort()
+
+dp = [1] * n
+
+for i in range(n):
+    for j in range(i):
+        if lines[i][1] > lines[j][1]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(n - max(dp))
