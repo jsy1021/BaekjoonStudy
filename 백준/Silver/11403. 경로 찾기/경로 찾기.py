cf6771835acf0bd1a8f90993f021b19910f77969
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2024-03-09 01:03:44 +0900

    [Silver I] Title: 경로 찾기, Time: 216 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,10 @@
+N=int(input())
+arr=[list(map(int,input().split())) for _ in range(N)]
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if arr[i][k] and arr[k][j]:
+                arr[i][j]=1
+for line in arr:
+    print(*line)
