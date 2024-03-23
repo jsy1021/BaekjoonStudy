0c51d1ab5881ff068b75746bc4b608beb278aa2f
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2024-03-24 03:00:13 +0900

    [Silver I] Title: 본대 산책, Time: 280 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/12849. 본대 산책/본대 산책.py
@@ -0,0 +1,17 @@
+D=int(input())
+dp=[0]*8
+dp[0]=1
+for i in range(D):
+    tmp = [0] * 8
+    tmp[0] = dp[1] + dp[2]
+    tmp[1] = dp[0] + dp[2] + dp[3]
+    tmp[2] = dp[0] + dp[1] + dp[3] + dp[4]
+    tmp[3] = dp[1] + dp[2] + dp[4] + dp[5]
+    tmp[4] = dp[2] + dp[3] + dp[5] + dp[6]
+    tmp[5] = dp[3] + dp[4] + dp[7]
+    tmp[6] = dp[4] + dp[7]
+    tmp[7] = dp[5] + dp[6]
+    
+    for j in range(8):
+        dp[j]=tmp[j]%1000000007
+print(dp[0])
