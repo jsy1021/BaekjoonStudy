720021825cbe88d7e0984c14bb635aaa45d61d48
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2024-04-07 22:54:04 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 264 ms, Memory: 49216 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+ 
+def find_parent(parent, a):
+    if parent[a] != a:
+        parent[a] = find_parent(parent, parent[a])
+    return parent[a]
+ 
+ 
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+ 
+ 
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+ 
+for i in range(1, v + 1):
+    parent[i] = i
+ 
+edges = []
+result = 0
+ 
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((a, b, cost))
+ 
+edges.sort(key=lambda x: x[2])
+ 
+for a, b, cost in edges:
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+ 
+print(result)
