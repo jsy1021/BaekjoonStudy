7959106b27e967bb523abe285f36a9e80a65f68c
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2024-04-11 03:48:34 +0900

    [Gold IV] Title: 플로이드, Time: 692 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,28 @@
+import sys
+input=sys.stdin.readline
+
+N=int(input())
+M=int(input())
+INF=int(1e9)
+graph=[[INF]*(N+1) for _ in range(N+1)]
+
+for _ in range(M):
+    a,b,cost=map(int,input().split())
+    graph[a][b]=min(cost,graph[a][b])
+    
+
+for i in range(1,N+1):
+    graph[i][i]=0
+
+for k in range(1,N+1):
+    for i in range(1,N+1):
+        for j in range(1,N+1):
+            graph[i][j]=min(graph[i][j],graph[i][k]+graph[k][j])
+
+for i in range(1,N+1):
+    for j in range(1,N+1):
+        if graph[i][j]==INF:
+            print("0", end=" ")
+        else:
+            print(graph[i][j],end=" ")
+    print()
