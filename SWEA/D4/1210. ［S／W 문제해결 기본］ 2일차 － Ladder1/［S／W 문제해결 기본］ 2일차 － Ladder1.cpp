22ae9dcb1fb07067f28d8402aa2089f8bc2d4946
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2024-11-15 01:34:09 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 32 ms, Memory: 13,556 KB -BaekjoonHub

---
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<vector<int>> arr(100, vector<int>(100));
+
+int findStartX(int startY, int startX) {
+    int x = startX;
+
+    // 도착점부터 위로 올라가면서 경로를 추적
+    while (startY >= 0) {
+        // 1. 오른쪽으로 이동할 수 있으면 계속 오른쪽으로 이동
+        if (x + 1 < 100 && arr[startY][x + 1] == 1) {
+            do {
+                x++;  // 오른쪽으로 이동
+            } while (x + 1 < 100 && arr[startY][x + 1] == 1);
+        }
+        // 2. 왼쪽으로 이동할 수 있으면 계속 왼쪽으로 이동
+        else if (x - 1 >= 0 && arr[startY][x - 1] == 1) {
+            do {
+                x--;  // 왼쪽으로 이동
+            } while (x - 1 >= 0 && arr[startY][x - 1] == 1);
+        }
+        // 3. 위로 한 칸 이동
+        startY--;
+    }
+
+    return x;  // 도착점에서 출발점까지의 x 좌표 반환
+}
+
+int main() {
+    for (int tc = 1; tc <= 10; tc++) {
+        int n, x, y;
+        cin >> n;
+
+        // 배열 입력
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                cin >> arr[i][j];
+            }
+        }
+
+        // 도착점 찾기 (arr[99][j]에서 2 찾기)
+        for (int j = 0; j < 100; j++) {
+            if (arr[99][j] == 2) {
+                x = j;
+                break;
+            }
+        }
+
+        // 도착점에서 출발점까지 x 좌표를 찾아서 출력
+        int startX = findStartX(99, x);
+        cout << "#" << n << " " << startX << endl;
+    }
+    return 0;
+}
