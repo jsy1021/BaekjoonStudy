984ee69ac411f54243fe574a52794b369fd14744
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2025-03-28 23:51:25 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1208 ms, Memory: 62076 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,21 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n= sc.nextInt();
-        int m=sc.nextInt();
+    public static void main(String[] args)throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+
+        int n= Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
         int arr[]=new int[n+1];
         int sum[]=new int[n+1];
+        st=new StringTokenizer(br.readLine());
         for(int i=1;i<=n;i++){
-            arr[i]=sc.nextInt();
+            arr[i]=Integer.parseInt(st.nextToken());
         }
         for(int i=1;i<=n;i++){ //구간 합 배열 완성
             sum[i]=arr[i]+sum[i-1];
         }
         for(int i=0;i<m;i++){
-            int start=sc.nextInt();
-            int end=sc.nextInt();
+            st=new StringTokenizer(br.readLine());
+            int start=Integer.parseInt(st.nextToken());
+            int end=Integer.parseInt(st.nextToken());
 
             System.out.println(sum[end]-sum[start-1]);
         }
