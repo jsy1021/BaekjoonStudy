4c9540987bb6c22aa01b9e765fc7fe43eb6275a2
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2025-03-29 01:02:32 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2648 ms, Memory: 328504 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n + 1][n + 1];
+        int[][] sum = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1] + arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+
+            int result = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1];
+            System.out.println(result);
+        }
+    }
+}
