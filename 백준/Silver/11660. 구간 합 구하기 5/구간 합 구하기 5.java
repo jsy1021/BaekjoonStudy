0f7a7f4ed915b7169ee07c455b5ac4fe393e7079
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2025-03-29 01:09:00 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 744 ms, Memory: 122892 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,33 +1,47 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        // 빠른 입력을 위해 BufferedReader 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
         int[][] arr = new int[n + 1][n + 1];
         int[][] sum = new int[n + 1][n + 1];
 
+        // 배열 입력 (빠르게 처리하기 위해 BufferedReader 사용)
         for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= n; j++) {
-                arr[i][j] = sc.nextInt();
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        // 누적 합 계산 (sum[x][y] 형식으로 저장)
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 sum[i][j] = sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1] + arr[i][j];
             }
         }
 
+        // 질의 처리
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
-            int x1 = sc.nextInt();
-            int y1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            int y2 = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
 
             int result = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1];
-            System.out.println(result);
+            sb.append(result).append("\n");
         }
+        
+        // 한 번에 출력하여 시간 단축
+        System.out.print(sb);
     }
 }
