732a16d5ae1d10db6c566447bc29fc263c6ed7cb
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2025-05-09 11:45:26 +0900

    [Silver I] Title: 절댓값 힙, Time: 524 ms, Memory: 28672 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,8 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+
 public class Main{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+   public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n=Integer.parseInt(br.readLine());
         PriorityQueue<Integer>pq=new PriorityQueue<>(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
@@ -15,7 +20,7 @@ public class Main{
             }
         });
         for(int i=0;i<n;i++){
-            int x=sc.nextInt();
+            int x=Integer.parseInt(br.readLine());
             if(x==0){
                 if(pq.isEmpty()){
                     System.out.println(0);
