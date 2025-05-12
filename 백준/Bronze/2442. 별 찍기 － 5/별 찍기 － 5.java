088c75494f929cc64caf36d82368e4a1cb71b3d5
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2025-05-12 22:21:56 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -2,17 +2,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 public class Main{
-    public static void main(String[] args)throws IOException {
+     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
+        StringBuilder sb=new StringBuilder();
         for(int i=1;i<=n;i++){
             for(int j=n-1-i;j>=0;j--){
-                System.out.print(" ");
+                sb.append(" ");
             }
             for(int k=0;k<2*i-1;k++){
-                System.out.print("*");
+                sb.append("*");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
