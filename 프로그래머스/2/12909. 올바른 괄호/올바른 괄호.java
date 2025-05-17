3a6eb7c27ae7f529f0b22e2b74e34548e1a87b93
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2025-05-17 19:21:46 +0900

    [level 2] Title: 올바른 괄호, Time: 21.82 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,20 +1,23 @@
-import java.util.Stack;
+import java.util.*;
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        Stack<Character>stack=new Stack<>();
-        for(char c: s.toCharArray()){
-            if(c=='('){
-                stack.push(')');
-            }
-            else if (stack.isEmpty() || stack.pop() != c){
-                return false;
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='(')
+                stack.push(c);
+            else if(c==')'){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
             }
         }
-        
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        
-
-        return stack.isEmpty();
+        if(stack.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
