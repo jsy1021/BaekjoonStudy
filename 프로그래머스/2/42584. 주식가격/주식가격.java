66125bb3d06e5bb56bf4639abcad681d70ae8c6b
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2025-05-17 21:48:22 +0900

    [level 2] Title: 주식가격, Time: 36.11 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,25 +1,25 @@
 import java.util.*;
 class Solution {
-	public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        Queue<Integer> queue=new LinkedList<>();
-        for(int i: prices){
-            queue.add(i);
-        }
-        int index=0;
-        while(!queue.isEmpty()){
-            int current=queue.poll();
-            for(int i=(prices.length-queue.size()); i<prices.length; i++){
-                if(current>prices[i]){
-                    answer[index]++;
-                    break;
-                }
-                if(current<=prices[i]){
-                    answer[index]++;
-                }
+    public int[] solution(int[] prices) {
+        int n = prices.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            // 현재 가격이 스택 top보다 낮아졌다면 answer에 계산
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int top = stack.pop();
+                answer[top] = i - top;
             }
-            index++;
+            stack.push(i);
         }
+
+        // 끝까지 가격이 떨어지지 않은 인덱스 처리
+        while (!stack.isEmpty()) {
+            int top = stack.pop();
+            answer[top] = n - 1 - top;
+        }
+
         return answer;
     }
 }
