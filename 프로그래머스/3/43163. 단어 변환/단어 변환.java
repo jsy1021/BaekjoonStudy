951a4076acd77f21c6a30d6019facc89efe2e2c2
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2025-05-29 17:02:51 +0900

    [level 3] Title: 단어 변환, Time: 0.64 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,46 +1,37 @@
 import java.util.*;
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        int n = words.length;
-        boolean targetInWords=false;
-        for(String word: words){
-            if(word.equals(target)){
-                targetInWords=true;
+        boolean targetInWords = false;
+        for (String word : words) {
+            if (word.equals(target)) {
+                targetInWords = true;
+                break;
             }
         }
-        if(!targetInWords)
-            return 0;
-        
-        Queue<Word> q=new LinkedList<>();
-        boolean [] visited=new boolean[n];
-        
-        q.add(new Word(begin,0));
-        
+        if (!targetInWords) return 0;
+
+        Queue<Word>q=new LinkedList<>();
+        boolean[] visited=new boolean[words.length];
+
+        q.add(new Word(begin,0));//시작 단어와 카운트 0을 큐에 넣음
+
         while(!q.isEmpty()){
             Word current=q.poll();
-            
+
             if(current.word.equals(target)){
                 return current.count;
             }
-            for(int i=0;i<n;i++){
+            for(int i=0;i<words.length;i++){
                 if(!visited[i]&&diff(current.word,words[i])){
                     visited[i]=true;
-                    q.add(new Word(words[i],current.count+1));
+                    q.add(new Word(words[i],current.count+1));//한 글자만 다른 단어는 count+1후 단어와 함께 큐에 add
                 }
             }
         }
-        return 0;
+
+    return 0;
     }
-    public class Word{
-        String word;
-        int count;
-        Word(String word, int count){
-            this.word=word;
-            this.count=count;
-        }
-    }
-    public boolean diff(String a, String b){
+    public boolean diff(String a,String b){
         int count=0;
         for(int i=0;i<a.length();i++){
             if(a.charAt(i)!=b.charAt(i))
@@ -50,6 +41,14 @@ class Solution {
             return true;
         else{
             return false;
+        }
+    }
+    public static class Word{
+        String word;
+        int count;
+        Word(String word, int count){
+            this.word=word;
+            this.count=count;
         }
     }
 }
