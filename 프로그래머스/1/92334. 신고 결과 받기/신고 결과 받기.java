6023507b2eac102d001d47dd93eabc695c644744
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2025-05-30 03:55:29 +0900

    [level 1] Title: 신고 결과 받기, Time: 188.52 ms, Memory: 156 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,32 +1,30 @@
 import java.util.*;
 class Solution {
-    public int[] solution(String[] id_list, String[] report, int k){
-        int []answer=new int [id_list.length];
-        //일단 중복 신고가 불가능함.->즉 report={신고자, 신고 대상자} 중복을 제거해야함.-> 중복을 제거하는건 Set
-        Set<String>reportSet=new HashSet<>(Arrays.asList(report));
-        //중복을 제거했으니 각 유저가 신고한 유저들의 리스트, 신고당한 횟수를 카운트하는 리스트를 만들어야함
-        Map<String, Integer>reportCount=new HashMap<>();
-        Map<String, List<String>>toFromList=new HashMap<>();
-        for(String s:reportSet){
-            String[] part=s.split(" ");
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int []answer=new int[id_list.length];
+        Set<String>report_set=new HashSet<>(Arrays.asList(report));
+        
+        Map<String,Integer>reportCount=new HashMap<>();
+        Map<String,List<String>>fromtoList=new HashMap<>();
+        for(String s:report_set){
+            String part[]=s.split(" ");
             String from=part[0];
             String to=part[1];
-
-            reportCount.put(to,reportCount.getOrDefault(to,0)+1);//to가 있다면 +1, 없으면 0(기본값)으로 초기화
-            if(!toFromList.containsKey(from)){
-                toFromList.put(from,new ArrayList<>());
+            
+            reportCount.put(to,reportCount.getOrDefault(to,0)+1);
+            if(!fromtoList.containsKey(from)){
+                fromtoList.put(from,new ArrayList<>());
             }
-            toFromList.get(from).add(to);//신고자:[신고당한 사람] 추가
+            fromtoList.get(from).add(to);
         }
         for(int i=0;i<id_list.length;i++){
-            String from=id_list[i];
-            //신고자가 신고한 리스트를 불러와야함
-            for(String to:toFromList.getOrDefault(from,new ArrayList<>())){
-                if(reportCount.get(to)>=k){
-                    answer[i]++;
-                }
+            String reporter=id_list[i];
+            for(String check: fromtoList.getOrDefault(reporter,new ArrayList<>())){
+                if(reportCount.get(check)>=k)
+                    answer[i]+=1;
             }
         }
+        
         return answer;
     }
 }
