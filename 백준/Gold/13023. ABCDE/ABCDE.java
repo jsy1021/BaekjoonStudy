116f327981708412a1196e02d918da7f1a0661c6
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2025-06-23 01:40:39 +0900

    [Gold V] Title: ABCDE, Time: 252 ms, Memory: 20864 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,20 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 public class Main{
     static List<List<Integer>>list =new ArrayList<>();
     static boolean [] visited;
     static int N,M;
     static boolean arrive;
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        N=sc.nextInt();
-        M=sc.nextInt();
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
         visited=new boolean[N];
         for(int i=0;i<N;i++){
             list.add(new ArrayList<>());
         }
+        
         for(int i=0;i<M;i++){
-            int a=sc.nextInt();
-            int b=sc.nextInt();
+            st=new StringTokenizer(br.readLine());
+            int a=Integer.parseInt(st.nextToken());
+            int b=Integer.parseInt(st.nextToken());
             list.get(a).add(b);
             list.get(b).add(a);
         }
