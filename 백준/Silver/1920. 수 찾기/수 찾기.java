cd46dea2f5cc7758b40a7af7746cac3241eed97f
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2025-06-27 14:50:16 +0900

    [Silver IV] Title: 수 찾기, Time: 1760 ms, Memory: 185068 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,43 +1,38 @@
 import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-public class Main {
-	
-	public static void main(String args[]){
-	   	
-		int i,j,n,m, temp;
-		Scanner sc=new Scanner(System.in);
-		n=sc.nextInt();
-		int arr[]=new int [n];
-		for(i=0;i<n;i++)
-		{
-			arr[i]=sc.nextInt();
-		}
-		m=sc.nextInt();
-		Arrays.sort(arr);
-		int arr2[]=new int [m];
-		for(i=0;i<m;i++) {
-			arr2[i]=sc.nextInt();
-		}
-		for(i=0;i<m;i++)
-		{
-			System.out.println(binarySearch(arr, arr2[i]));
-		}
-	}
-	public static int binarySearch(int arr[],int num)
-	{
-		int low=0;
-		int hi=arr.length-1;
-		int mid=0;
-		
-		while(low<=hi) {
-			mid=(low+hi)/2;
-			if(arr[mid]==num)
-				return 1;
-			else if(arr[mid]>num)
-				hi=mid-1;
-			else if(arr[mid]<num)
-				low=mid+1;
-		}
-		return 0;
-	}
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr); // 이진 탐색 전 정렬
+
+        int m = sc.nextInt();
+        for(int i = 0; i < m; i++) {
+            boolean find = false;
+            int target = sc.nextInt();
+            int start = 0;
+            int end = arr.length - 1;
+
+            while(start <= end) {
+                int mid_index = (start + end) / 2;
+                int mid_value = arr[mid_index];
+
+                if(mid_value < target) {
+                    start = mid_index + 1;
+                } else if(mid_value > target) {
+                    end = mid_index - 1;
+                } else {
+                    find = true;
+                    break;
+                }
+            }
+
+            System.out.println(find ? 1 : 0);
+        }
+    }
 }
