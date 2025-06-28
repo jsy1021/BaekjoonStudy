00cdc2fa0d59d897b39cd9ada95c1746c56a53ca
jsy1021 <95871024+jsy1021@users.noreply.github.com>
2025-06-29 05:46:25 +0900

    [Silver I] Title: 기타 레슨, Time: 284 ms, Memory: 24332 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,19 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 public class Main{
-     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+     public static void main(String[] args)throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         int arr[]=new int[n];
         int sum=0;
         int max=0;
+        st=new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+            arr[i]=Integer.parseInt(st.nextToken());
             if(max<arr[i])
                 max=arr[i];
             sum+=arr[i];
         }
-        
+
         int start=max;
         int end=sum;
         int answer=0;
